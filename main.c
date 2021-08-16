589b8077276221ef8654a28e50ee25f78ccf8023
mabroor007 <amabroor007@gmail.com>
2021-08-16 22:42:21 +0500

    first usefull program in c a temprature converter

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+void logTemprature(int farh, int cel) {
+  printf("%df is equal to %dc \n", farh, cel);
+}
+
+float getCelsiusFromFarh(int farh) { return (5 * (farh - 32)) / 9; }
+
 int main() {
-  printf("hello from c");
-  return 0;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  while (lower <= upper) {
+    int cel = (int)getCelsiusFromFarh(lower);
+    logTemprature(lower, cel);
+    lower = step + lower;
+  }
 }
